a71ffa4093941bb820e0579b56a17422b3ab0021
Aogon <aoi_sakai@icloud.com>
2020-05-15 17:10:46 +0900

    Step2 10問のクイズアプリにする

--- app/src/main/java/app/sakai/tororoimo/sosuu/MainActivity.kt
+++ app/src/main/java/app/sakai/tororoimo/sosuu/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -34,5 +36,79 @@ class MainActivity : AppCompatActivity() {
 
         textView.text = questions[answerCount].toString() + ""
         textView.setTextColor(Color.BLACK)
+    }
+
+    fun maru(v: View) {
+        var answer = true
+        val number = questions[answerCount]
+
+        for (i in 2 until number) {
+            if (number % i == 0) {
+                answer = false
+                break
+            }
+        }
+
+        if (answer) {
+            Toast.makeText(this, "正解", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "不正解", Toast.LENGTH_SHORT).show()
+        }
+
+        if (answer) {
+            point++
+            Log.d("maru", "正解" + point.toString())
+        } else {
+            Log.d("maru", "不正解")
+        }
+
+        answerCount ++
+        if (answerCount  == QUESTION_COUNT) {
+            textView.text = point.toString() + "点"
+            textView.setTextColor(Color.RED)
+
+            point = 0
+            answerCount = 0
+        } else {
+            textView.text = questions[answerCount].toString() + ""
+            textView.setTextColor(Color.BLACK)
+        }
+    }
+
+    fun batsu(v: View) {
+        var answer = true
+        val number = questions[answerCount]
+
+        for (i in 2 until number) {
+            if (number % i == 0) {
+                answer = false
+                break
+            }
+        }
+
+        if (answer) {
+            Toast.makeText(this, "不正解", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "正解", Toast.LENGTH_SHORT).show()
+        }
+
+        if (answer) {
+            Log.d("maru", "不正解")
+        } else {
+            point++
+            Log.d("maru", "正解" + point.toString())
+        }
+
+        answerCount ++
+        if (answerCount  == QUESTION_COUNT) {
+            textView.text = point.toString() + "点"
+            textView.setTextColor(Color.RED)
+
+            point = 0
+            answerCount = 0
+        } else {
+            textView.text = questions[answerCount].toString() + ""
+            textView.setTextColor(Color.BLACK)
+        }
     }
     }
